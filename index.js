5ead99b657cc753bff87cf1ab2c2172d52a84ecd
Jon Lambson <jonlambson@gmail.com>
2018-02-04 11:30:41 -0700

    fixing port for heroku

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ var express = require('express');
 
 // App Setup
 var app = express();
-var server = app.listen(3000, () => {
-  console.log('Listening on port 3000');
-});
+var server = app.listen(process.env.PORT || 5000)
 
 // Static Files
 app.use(express.static('public'));
